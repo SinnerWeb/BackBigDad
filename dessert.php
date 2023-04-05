a1f0fae6ccedae2b623c508550af6a4c91e27a6f
SinnerWeb <dwesh5030@gmail.com>
2023-04-05 21:39:03 +0300

    lol

---
+++ dessert.php
@@ -0,0 +1,20 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+
+require_once '/OSPanel/domains/test1.com/db.php';
+
+$sql = "SELECT id, name_product, descrip, price, img_url FROM dessertproduct";
+$result = $mysql->query($sql);
+
+$data = array();
+if ($result->num_rows > 0) {
+   while ($row = $result->fetch_assoc()) {
+      $data[] = $row;
+   }
+}
+
+
+$json_data = json_encode($data);
+
+header('Content-type: application/json');
+echo $json_data;
